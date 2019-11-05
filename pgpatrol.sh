ebbedfa9e295bb547d586b03a2e3965af3be1ca1
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-05 22:23:45 +0100

    Update pgpatrol.sh

--- pgpatrol.sh
+++ pgpatrol.sh
@@ -6,7 +6,6 @@
 # GNU:        General Public License v3.0
 ################################################################################
 source /opt/plexguide/menu/functions/functions.sh
-source /opt/plexguide/menu/functions/watchtower.sh
 source /opt/plexguide/menu/functions/install.sh
 # KEY VARIABLE RECALL & EXECUTION
 mkdir -p /var/plexguide/pgpatrol
