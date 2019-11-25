ad301ad3806045b8261fba55a2bbd8989c0ad845
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-25 23:18:35 +0100

    update repo

--- pgpatrol.sh
+++ pgpatrol.sh
@@ -1,300 +1,31 @@
 #!/bin/bash
 #
 # Title:      PGBlitz (Reference Title File)
-# Author(s):  Admin9705
+# Author(s):  Admin9705 - Deiteq
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-source /opt/plexguide/menu/functions/functions.sh
-source /opt/plexguide/menu/functions/install.sh
-# KEY VARIABLE RECALL & EXECUTION
-mkdir -p /var/plexguide/pgpatrol
-
-# FUNCTIONS START ##############################################################
-
-# FIRST FUNCTION
-variable() {
-  file="$1"
-  if [ ! -e "$file" ]; then echo "$2" >$1; fi
-}
-
-doneenter(){
- echo
-  read -p 'All done | PRESS [ENTER] ' typed </dev/tty
-  question1
-}
-
-deploycheck() {
-  dcheck=$(systemctl is-active pgpatrol.service)
-  if [ "$dcheck" == "active" ]; then
-    dstatus="✅ DEPLOYED"
-  else dstatus="⚠️ NOT DEPLOYED"; fi
-}
-
-plexcheck() {
-  pcheck=$(docker ps --format {{.Names}} | grep "plex")
-  if [ "$pcheck" == "" ]; then
-
-    tee <<-EOF
+# Menu Interface
+question1(){
+tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⛔️  WARNING! - Plex is Not Installed or Running! Exiting!
+📂  Sorry we removed this repo 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-EOF
-    read -p 'Confirm Info | PRESS [ENTER] ' typed </dev/tty
-    exit
-  fi
-}
+sorry, but this repo does not exist anymore, 
 
-token() {
-  touch /var/plexguide/plex.token
-  ptoken=$(cat /var/plexguide/plex.token)
-  if [ "$ptoken" == "" ]; then
-    bash /opt/plexguide/menu/plex/token.sh
-    ptoken=$(cat /var/plexguide/plex.token)
-    if [ "$ptoken" == "" ]; then
-      tee <<-EOF
+please update your PTS version, thanks
 
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-⛔️  WARNING! - Failed to Generate a Valid Plex Token! Exiting Deployment!
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-EOF
-      read -p 'Confirm Info | PRESS [ENTER] ' typed </dev/tty
-      exit
-    fi
-  fi
-}
-
-# BAD INPUT
-badinput() {
-  echo
-  read -p '⛔️ ERROR - BAD INPUT! | PRESS [ENTER] ' typed </dev/tty
-  question1
-}
-selection1() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Instantly Kick Video Transcodes?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-[1] False
-[2] True
+[Z] - Exit
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
 
+# Standby
   case $typed in
-  1) echo -e "false" >/var/plexguide/pgpatrol/video.transcodes && question1 ;;
-  2) echo -e "true" >/var/plexguide/pgpatrol/video.transcodes && question1 ;;
-  *) badinput ;;
-  esac
-}
-selection2() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Instantly Kick 4k - Video Transcodes?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-[1] False
-[2] True
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-
-  case $typed in
-  1) echo -e "false" >/var/plexguide/pgpatrol/video.transcodes4k && question1 ;;
-  2) echo -e "true" >/var/plexguide/pgpatrol/video.transcodes4k && question1 ;;
-  *) badinput ;;
-  esac
-}
-selection3() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Instantly Kick Audio Transcodes?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-[1] False
-[2] True
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-
-  case $typed in
-  1) echo -e "false" >/var/plexguide/pgpatrol/audio.transcodes && question1 ;;
-  2) echo -e "true" >/var/plexguide/pgpatrol/audio.transcodes && question1 ;;
-  *) badinput ;;
-  esac
-}
-selection4() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Limit Amount of Different IPs a User Can Make?
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-Set a Number from [ 1 ] - [ 10 ]
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-  if [[ "$typed" -ge "1" && "$typed" -le "10" ]]; then
-    echo -e "$typed" >/var/plexguide/pgpatrol/multiple.ips && question1
-  else badinput; fi
-}
-selection5() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Limit How Long a User Can Pause For!
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-Set a Number from [ 5 ] - [ 250 ] Mintues
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-  if [[ "$typed" -ge "1" && "$typed" -le "250" ]]; then
-    echo -e "$typed" >/var/plexguide/pgpatrol/kick.minutes && question1
-  else badinput; fi
-}
-selection6() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Check Interval # how often to check the active streams in seconds
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-Set a Number from [ 60 ] - [ 240 ] seconds
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-  if [[ "$typed" -ge "59" && "$typed" -le "241" ]]; then
-    echo -e "$typed" >/var/plexguide/pgpatrol/check.interval && question1
-  else badinput; fi
-}
-selection7() {
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Remove Plex Patrol  || l3uddz/plex_patrol
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-[ 1 ] - NO
-
-[ 2 ] - YES
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-EOF
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-
- case $typed in
- 1) question1 ;; 
- 2) ansible-playbook /opt/pgpatrol/remove-pgpatrol.yml && question1 ;;
- *) badinput ;;
- esac
-}
-credits(){
-clear
-chk=$(figlet Plex Patrol | lolcat )
-
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Plex Patrol Credits 
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-$chk
-
-#########################################################################
-# Author:   l3uddz                                                      #
-# URL:      https://github.com/l3uddz/plex_patrol                       #
-# Coder of plex_patrol                                                  #
-# --                                                                    #
-# Author(s):     l3uddz, desimaniac                                     #
-# URL:           https://github.com/cloudbox/cloudbox                   #
-# --                                                                    #
-#         Part of the Cloudbox project: https://cloudbox.works          #
-#########################################################################
-#                   GNU General Public License v3.0                     #
-#########################################################################
-EOF
-
- echo
-  read -p 'Confirm Info | PRESS [ENTER] ' typed </dev/tty
-  clear && question1
-}
-# FIRST QUESTION
-question1() {
-deploycheck
-
-  video=$(cat /var/plexguide/pgpatrol/video.transcodes)
-  video4k=$(cat /var/plexguide/pgpatrol/video.transcodes4k)
-  ips=$(cat /var/plexguide/pgpatrol/multiple.ips)
-  minutes=$(cat /var/plexguide/pgpatrol/kick.minutes)
-  audio=$(cat /var/plexguide/pgpatrol/audio.transcodes)
-  interval=$(cat /var/plexguide/pgpatrol/check.interval)
-
-  tee <<-EOF
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚀 Plex - Patrol Interface || l3uddz/plex_patrol
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-
-[1] Instantly Kick Video Transcodes?      [ $video ]
-[2] Instantly Kick Video 4k Transcodes?   [ $video4k ]
-[3] Instantly Kick Audio Transcodes?      [ $audio ]
-[4] Multiple IPs for UserName             [ $ips ]
-[5] Minutes  | Kick Paused Transcode?     [ $minutes ]
-[6] Check Interval                        [ $interval ]
-
-[7] Deploy Plex - Patrol                  [ $dstatus ]
-[8] Remove Plex - Patrol
-
-[C] Credits
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-[Z] Exit
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-EOF
-
-  read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
-
-  case $typed in
-  1) selection1 && clear && question1 ;;
-  2) selection2 && clear && question1 ;;
-  3) selection3 && clear && question1 ;;
-  4) selection4 && clear && question1 ;;
-  5) selection5 && clear && question1 ;;
-  6) selection6 && clear && question1 ;;
-  7) ansible-playbook /opt/pgpatrol/pgpatrol.yml && sleep 5 && clear && question1 ;;
-  8) selection7 && question1 ;;
-  C) credits && clear && question1 ;;
-  c) credits && clear && question1 ;;
   z) exit ;;
   Z) exit ;;
   *) question1 ;;
   esac
 }
-
-# FUNCTIONS END ##############################################################
-plexcheck
-token
-variable /var/plexguide/pgpatrol/video.transcodes "NON-SET"
-variable /var/plexguide/pgpatrol/video.transcodes4k "NON-SET"
-variable /var/plexguide/pgpatrol/audio.transcodes "NON-SET"
-variable /var/plexguide/pgpatrol/check.interval "NON-SET"
-variable /var/plexguide/pgpatrol/multiple.ips "NON-SET"
-variable /var/plexguide/pgpatrol/kick.minutes "NON-SET"
-deploycheck
-question1
